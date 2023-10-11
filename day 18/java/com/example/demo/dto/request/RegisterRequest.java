dfa20f80e8cf85998f385cf0725ca32843f152a9
Barath0011 <120083493+Barath0011@users.noreply.github.com>
2023-10-11 11:47:36 +0530

    Add files via upload

---
+++ day 18/java/com/example/demo/dto/request/RegisterRequest.java
@@ -0,0 +1,42 @@
+package com.example.demo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+	  private String username;
+	  public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String emailid;
+	  private String mobileno;
+	  private String password;
+
+}
