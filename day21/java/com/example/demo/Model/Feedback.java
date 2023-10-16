eb57ffc5d6362f5a179373207fb3353d38bd1409
Barath0011 <120083493+Barath0011@users.noreply.github.com>
2023-10-16 12:18:18 +0530

    Add files via upload

---
+++ day21/java/com/example/demo/Model/Feedback.java
@@ -0,0 +1,29 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="feedback")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Feedback 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int feedbackId;
+	private  String name;
+	private  String email;
+	private String feedback;
+
+	
+}
